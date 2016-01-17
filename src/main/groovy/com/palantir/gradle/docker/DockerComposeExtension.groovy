4110dcf86c685a2a1c0aad56b859c5f4e06a5753
Mark Elliot <mark.elliot@gmail.com>
2016-01-16 19:34:09 -0800

    Add missing license headers

--- src/main/groovy/com/palantir/gradle/docker/DockerComposeExtension.groovy
+++ src/main/groovy/com/palantir/gradle/docker/DockerComposeExtension.groovy
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * <http://www.apache.org/licenses/LICENSE-2.0>
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,8 +15,9 @@
  */
 package com.palantir.gradle.docker
 
-import com.google.common.base.Preconditions
 import org.gradle.api.Project
+
+import com.google.common.base.Preconditions
 
 class DockerComposeExtension {
     Project project
