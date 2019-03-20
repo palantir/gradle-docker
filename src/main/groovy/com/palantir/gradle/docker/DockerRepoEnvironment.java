2086b582d6f3cbdbc8a414a15ec1a1653af09dfd
Christoph Birk <christoph.birk@gmail.com>
2019-03-20 20:40:00 +0100

    Extend repo configuration with username and password

--- src/main/groovy/com/palantir/gradle/docker/DockerRepoEnvironment.java
+++ src/main/groovy/com/palantir/gradle/docker/DockerRepoEnvironment.java
@@ -7,21 +7,41 @@ public class DockerRepoEnvironment {
 
     private final String name;
     private Property<String> url;
+    private Property<String> user;
+    private Property<String> password;
 
     public DockerRepoEnvironment(String name, ObjectFactory objectFactory) {
         this.name = name;
         this.url = objectFactory.property(String.class);
-    }
-
-    public void setUrl(String url) {
-        this.url.set(url);
+        this.user = objectFactory.property(String.class);
+        this.password = objectFactory.property(String.class);
     }
 
     public String getName() {
         return name;
     }
 
+    public void setUrl(String url) {
+        this.url.set(url);
+    }
+
     public Property<String> getUrl() {
         return url;
+    }
+
+    public Property<String> getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user.set(user);
+    }
+
+    public Property<String> getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password.set(password);
     }
 }
