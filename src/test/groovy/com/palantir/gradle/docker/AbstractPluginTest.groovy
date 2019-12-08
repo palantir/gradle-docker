b194aaced5182a14b42e30587532b4bf69d5da61
Tom Gregory <t.k.gregory@gmail.com>
2019-12-08 21:57:31 +0000

    Add task for docker compose down.

--- src/test/groovy/com/palantir/gradle/docker/AbstractPluginTest.groovy
+++ src/test/groovy/com/palantir/gradle/docker/AbstractPluginTest.groovy
@@ -79,6 +79,16 @@ class AbstractPluginTest extends Specification {
         }
     }
 
+    protected int processCount() {
+        StringBuffer sout = new StringBuffer(), serr = new StringBuffer()
+        Process proc = 'docker ps -q'.execute()
+        proc.consumeProcessOutput(sout, serr)
+        proc.waitFor()
+        assert proc.exitValue() == 0
+
+        return sout.readLines().size()
+    }
+
     protected String escapePath(String path) {
         return path.replaceAll('\\\\', '\\\\\\\\')
     }
